ec0d03918bb0499565ce8aa184fcbb1a9e9531f2
Martin Debrunne <martin.debrunne@gmail.com>
2017-08-27 15:15:15 +0200

    prepare devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable
+  
   has_many :workers
   has_many :products, through: :workers
 end
