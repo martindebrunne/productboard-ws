4ff13cc53fddccb319c1988cca1bd3e2d832e92e
Martin Debrunne <martin.debrunne@gmail.com>
2017-08-28 06:25:44 +0200

    heroku test2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,10 +36,10 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = "wss://productboad-ws.herokuapp.com/cable"
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
   config.web_socket_server_url = "wss://productboad-ws.herokuapp.com/cable" 
-  config.action_cable.allowed_request_origins = ['https://productboad-demo.herokuapp.com']
+  config.action_cable.allowed_request_origins = ['https://productboad-demo.herokuapp.com', 'https://productboad-demo.herokuapp.com']
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
